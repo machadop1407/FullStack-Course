fcbfdf14e2a8d9ef364d7f5e2cffbfc66ce9efc0
Pedro Machado <machadop1407@gmail.com>
2021-04-06 19:48:17 -0700

    Ep4

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
 import "./App.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
-  const [listOfPosts, setListOfPosts] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/posts").then((response) => {
-      setListOfPosts(response.data);
-    });
-  }, []);
   return (
     <div className="App">
-      {listOfPosts.map((value, key) => {
-        return (
-          <div className="post">
-            <div className="title"> {value.title} </div>
-            <div className="body">{value.postText}</div>
-            <div className="footer">{value.username}</div>
-          </div>
-        );
-      })}
+      <Router>
+        <Link to="/createpost"> Create A Post</Link>
+        <Link to="/"> Home Page</Link>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/createpost" exact component={CreatePost} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function Home() {
+  const [listOfPosts, setListOfPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setListOfPosts(response.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      {listOfPosts.map((value, key) => {
+        return (
+          <div className="post">
+            <div className="title"> {value.title} </div>
+            <div className="body">{value.postText}</div>
+            <div className="footer">{value.username}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Home;
