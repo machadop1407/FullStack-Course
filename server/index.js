def2575531d86833ba364458dc48203450ceecb2
Pedro Machado <machadop1407@gmail.com>
2021-04-12 23:50:52 -0700

    Ep6

--- server/index.js
+++ server/index.js
@@ -10,6 +10,8 @@ const db = require("./models");
 // Routers
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
+const commentsRouter = require("./routes/Comments");
+app.use("/comments", commentsRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(3001, () => {
